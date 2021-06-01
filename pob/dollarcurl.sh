f1560aeb8482efee41914ca718360c3aafa549c3
achimmertens <Ranzi@t-online.de>
2021-06-01 08:55:33 +0200

    coinmarket api curl command added for HIVE

--- pob/dollarcurl.sh
+++ pob/dollarcurl.sh
@@ -1,1 +1,3 @@
 # hello world
+curl -H "X-CMC_PRO_API_KEY: a1ff4bd0-2ac9-4700-ae61-6eaa62f56adc" -H "Accept: application/json" -d "symbol=HIVE" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/info
+
