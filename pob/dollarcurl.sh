0c99f7906d3cfa1c85fbeb45bcae0e4f60f072e3
achimmertens <Ranzi@t-online.de>
2021-06-02 08:07:03 +0200

    added awk command

--- pob/dollarcurl.sh
+++ pob/dollarcurl.sh
@@ -1,3 +1,3 @@
 # hello world
-curl -H "X-CMC_PRO_API_KEY: a1ff4bd0-2ac9-4700-ae61-6eaa62f56adc" -H "Accept: application/json" -d "symbol=HIVE" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/info
-
+curl -H "X-CMC_PRO_API_KEY: a1ff4bd0-2ac9-4700-ae61-6eaa62f56adc" -H "Accept: application/json" -d "symbol=HIVE" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/info > coinmarketcap.tmp
+cat coinmarketcap.tmp  | awk -F'price of Hive is' '{print $2}' | awk -F'USD ' '{print $1}' > hiveprice.tmp
