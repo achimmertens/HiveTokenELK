e9329bb6ccdbb1418f283bbeab94dd24449078e8
achimmertens <Ranzi@t-online.de>
2021-06-08 08:31:21 +0200

    Pobprice is now also as index.html available

--- pob/dollarcurl.sh
+++ pob/dollarcurl.sh
@@ -3,9 +3,13 @@
 cat coinmarketcap.tmp  | awk -F'price of Hive is' '{print $2}' | awk -F'USD ' '{print $1}' > hiveprice.tmp
 HIVEPRICE=$(cat hiveprice.tmp)
 echo "The price of \$HIVE/\$USD = "$HIVEPRICE
+echo "The price of \$HIVE/\$USD = "$HIVEPRICE > pobdollar.tmp
 read HIVEPRICE < hiveprice.tmp
 cat pobcurl3.log | awk -F'price\":\"' '{print $2}' | awk -F'\"' '{print $1}' > pobprice.tmp
 read POBPRICE < pobprice.tmp
 echo "The price of \$POB/\$HIVE = " $POBPRICE
+echo ". The price of \$POB/\$HIVE = " $POBPRICE >> pobdollar.tmp
 POBDOLLAR=`echo $POBPRICE \* $HIVEPRICE|bc`
 echo "The price of \$POB/\$USD = "$POBDOLLAR
+echo ". The price of \$POB/\$USD = "$POBDOLLAR >> pobdollar.tmp
+sudo cp pobdollar.tmp /var/www/html/elk/index.html
