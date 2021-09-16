a17d4d5637c7c37c4bddc7428950bd86b99cfe05
achimmertens <Ranzi@t-online.de>
2021-09-16 08:21:21 +0200

    setup kibana geändert - der hatte wohl einen dicken Fehler beim Bulkload der token, den ich vorher nich bemerkt hatte.

--- setup_kibana.sh
+++ setup_kibana.sh
@@ -21,15 +21,18 @@ curl --location --request PUT 'http://localhost:9200/beerbot' --header 'Content-
 curl --location --request PUT 'http://localhost:9200/spt' --header 'Content-Type: application/json' --data-bin @put_index.json
 
 # bulkload the consolidated data for each token
-# (Question: Shouldn't I rename "token" in the URL into the real tokenname? I think it worked last time like it is, but I don't know why)
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/beer/log/beercurlcons.log
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/chary/log/charycurlcons.log
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/leo/log/leocurlcons.log
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/list/log/listcurlcons.log
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/pob/log/pobcurlcons.log
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/sim/log/simcurlcons.log
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/beer/logbot/beerbotcurlcons.log
-curl --location --request POST 'http://localhost:9200/token/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/spt/log/sptcurlcons.log
+curl --location --request POST 'http://localhost:9200/beer/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/beer/log/beercurlcons.log
+curl --location --request POST 'http://localhost:9200/chary/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/chary/log/charycurlcons.log
+curl --location --request POST 'http://localhost:9200/leo/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/leo/log/leocurlcons.log
+curl --location --request POST 'http://localhost:9200/list/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/list/log/listcurlcons.log
+curl --location --request POST 'http://localhost:9200/pob/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/pob/log/pobcurlcons.log
+curl --location --request POST 'http://localhost:9200/sim/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/sim/log/simcurlcons.log
+curl --location --request POST 'http://localhost:9200/beerbot/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/beer/logbot/beerbotcurlcons.log
+curl --location --request POST 'http://localhost:9200/spt/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/spt/log/sptcurlcons.log
 
 # Now upload "All_kibana_objects.ndjson
 # (maybe?) create the index patternview for each indice
+
+# This is new and under construction:
+# curl --location --request PUT 'http://localhost:9200/dollartoken' --header 'Content-Type: application/json' --data-bin @/home/pi/elk/chary/dollarcharyindex.json
+# curl --location --request POST 'http://localhost:9200/dollartoken/_bulk?' --header 'Content-Type: application/json' --data-binary @/home/pi/elk/chary/log/dollarchary.ndjson
