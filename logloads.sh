6c36e11b3293391486afa83e56ab0696fa61b031
achimmertens <Ranzi@t-online.de>
2022-04-04 06:42:59 +0200

    new logs

---
+++ logloads.sh
@@ -0,0 +1,17 @@
+# taken from crontab -l 
+/home/pi/elk/beer/beercurl_json.sh >> /home/pi/elk/beer/log/cron.log
+/home/pi/elk/chary/charycurl_json.sh >> /home/pi/elk/chary/log/cron.log
+/home/pi/elk/pob/pobcurl_json.sh >> /home/pi/elk/pob/log/cron.log
+/home/pi/elk/list/listcurl_json.sh >> /home/pi/elk/list/log/cron.log
+/home/pi/elk/leo/leocurl_json.sh >> /home/pi/elk/leo/log/cron.log
+/home/pi/elk/sim/simcurl_json.sh >> /home/pi/elk/sim/log/cron.log
+/home/pi/elk/beer/botcurl_json.sh >> /home/pi/elk/beer/logbot/cron.log
+/home/pi/elk/spt/sptcurl_json.sh >> /home/pi/elk/spt/log/cron.log
+/home/pi/elk/chary/dollarchary.sh >> /home/pi/elk/chary/log/cron.log
+/home/pi/elk/beer/dollarbeer.sh >> /home/pi/elk/beer/log/cron.log
+/home/pi/elk/leo/dollarleo.sh >> /home/pi/elk/leo/log/cron.log
+/home/pi/elk/list/dollarlist.sh >> /home/pi/elk/list/log/cron.log
+/home/pi/elk/pob/dollarpob.sh >> /home/pi/elk/pob/log/cron.log
+/home/pi/elk/sim/dollarsim.sh >> /home/pi/elk/sim/log/cron.log
+/home/pi/elk/spt/dollarspt.sh >> /home/pi/elk/spt/log/cron.log
+
