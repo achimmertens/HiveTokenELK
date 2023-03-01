bc6592cb69d127afc81048d9c3aaa63020c32da5
achimmertens <achim_mertens@gmx.de>
2023-03-01 07:35:44 +0100

    einige Restore-Scripte angepasst

--- elk_start.sh
+++ elk_start.sh
@@ -3,7 +3,7 @@
 # ------------------- Steart ELK ------------------
 # This file is to start Elasticsearch in a docker container
 # docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v /usr/share/elasticsearch/data:/usr/share/elasticsearch/data comworkio/elasticsearch:latest-arm
-docker start 86bd
+docker start elasti
 docker start hammurabi
 sudo service kibana start
 
