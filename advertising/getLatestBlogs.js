b937286e9ee5e3c41762ef5e6182c6039245d1ac
Achim Mertens <achim_mertens@gmx.de>
2023-05-30 12:18:14 +0200

    Der Filter funktioniert und speichert ausgewählte Felder in die log.txt Datei

--- advertising/getLatestBlogs.js
+++ advertising/getLatestBlogs.js
@@ -6,7 +6,7 @@ const url = 'https://api.hive.blog';
 const requestData = {
   jsonrpc: '2.0',
   method: 'condenser_api.get_discussions_by_trending',
-  params: [{ tag: 'hive', limit: 5 }],
+  params: [{ tag: 'test', limit: 20 }],
   id: 1,
 };
 
@@ -17,9 +17,24 @@ axios.post(url, requestData, {
   },
 })
   .then(response => {
-    const result = JSON.stringify(response.data);
-    fs.writeFileSync('log.txt', result); // Schreibe das Ergebnis in die Log-Datei
-    console.log('API-Antwort erfolgreich in die Log-Datei geschrieben.');
+    const result = response.data.result;
+    const filteredPosts = result.filter(post => post.body.includes('Achim is cool'));
+
+    // Felder auswählen und in die Log-Datei schreiben
+    const logData = filteredPosts.map(post => {
+      return {
+        author: post.author,
+        permlink: post.permlink,
+        title: post.title,
+        created: post.created,
+        url: post.url,
+        post_id: post.post_id,
+      };
+    });
+
+    fs.writeFileSync('log.txt', JSON.stringify(logData, null, 2));
+
+    console.log('Gefilterte Posts erfolgreich in die Log-Datei geschrieben.');
   })
   .catch(error => {
     console.error('Fehler bei der API-Anfrage:', error);
