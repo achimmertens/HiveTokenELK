834fbb817261131db6ce42f3c6d44ac4e5bf07f3
Achim Mertens <achim_mertens@gmx.de>
2023-09-06 07:29:37 +0200

    feat: creates post on Steem.

--- advertising/post.js
+++ advertising/post.js
@@ -1,21 +1,24 @@
-import fs from 'fs';
-import hive from '@hiveio/hive-js';
-import config from './config.js';
+const fs = require('fs');
+const hive = require('@hiveio/hive-js');
+const steem = require('steem');
+const config = require('./config.js');
 
 const privateKey = config.privateKey;
-hive.api.setOptions({ url: 'https://api.hive.blog' });
+//hive.api.setOptions({ url: 'https://api.hive.blog' });
+hive.api.setOptions({ url: 'https://api.steem.blog' });
 
 const parentAuthor = ''; // Leer lassen, da es sich um einen eigenständigen Post handelt
-const parentPermlink = 'eine-kopie-von-achimmertens'; // Permlink des Elternbeitrags, kann frei gewählt werden
-const author = 'anobel';
+const parentPermlink = 'ein-test-von-achimmertens'; // Permlink des Elternbeitrags, kann frei gewählt werden
+const author = 'achimmertens';
 const permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
-const title = 'Eine Kopie von achimmertens';
+const title = 'Eine Test von achimmertens';
 const bodyFilePath = './body.md';
 
 // Den Inhalt der body.md-Datei lesen
 const body = fs.readFileSync(bodyFilePath, 'utf-8');
 
-hive.broadcast.comment(
+//hive.broadcast.comment(
+steem.broadcast.comment(  
   privateKey,
   parentAuthor,
   parentPermlink,
@@ -25,6 +28,10 @@ hive.broadcast.comment(
   body,
   { tags: ['test'], app: 'test/0.1' },
   function(err, result) {
-    console.log(err, result);
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Post erfolgreich erstellt:', result);
+    }
   }
 );
